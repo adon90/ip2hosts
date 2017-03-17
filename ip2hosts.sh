db607916e013b2c210d388631cdde5f8d5834a39
Daniel Martinez Adan <dmartinez@sia.es>
2017-03-17 06:11:02 -0400

    arguments

--- ip2hosts.sh
+++ ip2hosts.sh
@@ -1,3 +1,7 @@
+if [ "$#" -ne 1 ]; then
+    echo "ip2hosts <ip>"
+	exit 1
+fi
 ip2hosts() { 
 	curl https://www.robtex.com/ip-lookup/$1 --silent  | grep "xsha" | grep -Po "(?<=dns-lookup/).*?(?=\">)" >> /tmp/domains.txt
 	curl "http://www.virustotal.com/vtapi/v2/ip-address/report?ip=$1&apikey=3c052e9a7339f3a73f00bd67baea747e47f59ee6c1596e59590fd953d00ce519" -s | json_pp | grep -Po "(?<=\"hostname\" : \").*?(?=\",)" >> /tmp/domains.txt
