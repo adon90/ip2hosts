b8c91618f51f741bd2a193b2e0cbeaf86d8673f4
aDoN <danysmith90@gmail.com>
2017-03-20 15:07:09 +0100

    rm domains.txt

--- ip2hosts.sh
+++ ip2hosts.sh
@@ -3,6 +3,7 @@ if [ "$#" -ne 1 ]; then
 	exit 1
 fi
 ip2hosts() { 
+	rm /tmp/domains.txt 2>/dev/null
 	curl https://www.robtex.com/ip-lookup/$1 --silent  | grep "xsha" | grep -Po "(?<=dns-lookup/).*?(?=\">)" >> /tmp/domains.txt
 	curl "http://www.virustotal.com/vtapi/v2/ip-address/report?ip=$1&apikey=3c052e9a7339f3a73f00bd67baea747e47f59ee6c1596e59590fd953d00ce519" -s | json_pp | grep -Po "(?<=\"hostname\" : \").*?(?=\",)" >> /tmp/domains.txt
 	dig +short -x $1 >> /tmp/domains.txt
