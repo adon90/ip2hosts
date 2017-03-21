0e8d8c9a823cf46699863c59fbb207364f20b858
aDoN <danysmith90@gmail.com>
2017-03-21 10:39:28 +0100

    Update ip2hosts.sh

--- ip2hosts.sh
+++ ip2hosts.sh
@@ -7,7 +7,7 @@ ip2hosts() {
 	rm /tmp/domains.txt 2>/dev/null
 	curl https://www.robtex.com/ip-lookup/$1 --silent  | grep "xsha" | grep -Po "(?<=dns-lookup/).*?(?=\">)" >> /tmp/domains.txt
 	curl "http://www.virustotal.com/vtapi/v2/ip-address/report?ip=$1&apikey=3c052e9a7339f3a73f00bd67baea747e47f59ee6c1596e59590fd953d00ce519" -s | json_pp | grep -Po "(?<=\"hostname\" : \").*?(?=\",)" >> /tmp/domains.txt
-    dig +short -x $1 >> /tmp/domains.txt
+        dig +short -x $1 >> /tmp/domains.txt
 	shodan host $1 2>/dev/null | grep -Po "(?<=Hostnames:\s).*" | tr -d " " >> /tmp/domains.txt
 	for i in {0..9}; do curl "https://www.bing.com/search?q=ip%3a$1&first=$i1" -s |  grep -Po "(?<=<a href=\").*?(?= h=)" | grep -Po "(?<=://).*?(?=/)" | egrep -v "microsoft"; done >> /tmp/domains.txt
 	nmap -p443 --script ssl-cert $1 2>/dev/null | grep -Po "(?<=commonName=).*?(?=/)" | tr '[:upper:]' '[:lower:]' >> /tmp/domains.txt
