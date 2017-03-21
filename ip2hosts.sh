6cdd2f749bd5e708a9300c5482aed8f49af0b928
Daniel Martinez Adan <dmartinez@sia.es>
2017-03-21 05:51:29 -0400

    mejorabing

--- ip2hosts.sh
+++ ip2hosts.sh
@@ -9,8 +9,8 @@ ip2hosts() {
 	curl "http://www.virustotal.com/vtapi/v2/ip-address/report?ip=$1&apikey=3c052e9a7339f3a73f00bd67baea747e47f59ee6c1596e59590fd953d00ce519" -s | json_pp | grep -Po "(?<=\"hostname\" : \").*?(?=\",)" >> /tmp/domains.txt
     dig +short -x $1 >> /tmp/domains.txt
 	shodan host $1 2>/dev/null | grep -Po "(?<=Hostnames:\s).*" | tr -d " " >> /tmp/domains.txt
-	for i in {0..9}; do curl "https://www.bing.com/search?q=ip%3a$1&first=$i1" -s |  grep -Po "(?<=<a href=\").*?(?= h=)" | grep -Po "(?<=://).*?(?=/)" | egrep -v "microsoft"; done >> /tmp/domains.txt
-	nmap -p443 --script ssl-cert $1 2>/dev/null | grep -Po "(?<=commonName=).*?(?=/)" | tr '[:upper:]' '[:lower:]' >> /tmp/domains.txt
+	for i in {0..9}; do curl "https://www.bing.com/search?q=ip%3a$1&first=$i1" -s |  grep -Po "(?<=<a href=\").*?(?= h=)" | grep -Po "(?<=://).*?(?=/)" | egrep -v "microsoft|bing|pointdecontact"; done >> /tmp/domains.txt
+	nmap -p443 --script ssl-cert $1 2>/dev/null | grep Subject | grep -Po "(?<=commonName=).*?(?=/)" | tr '[:upper:]' '[:lower:]' >> /tmp/domains.txt
 	sed -i 's/\.$//g' /tmp/domains.txt
 	sort -u /tmp/domains.txt
  }
