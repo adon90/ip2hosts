048f35190bbf0619df6a4a2514d1adb53cd5b34e
Daniel Martinez Adan <dmartinez@sia.es>
2017-03-21 04:09:46 -0400

    nmap ssl-cert

--- ip2hosts.sh
+++ ip2hosts.sh
@@ -9,6 +9,7 @@ ip2hosts() {
 	dig +short -x $1 >> /tmp/domains.txt
 	shodan host $1 2>/dev/null | grep -Po "(?<=Hostnames:\s).*" | tr -d " " >> /tmp/domains.txt
 	for i in {0..9}; do curl "https://www.bing.com/search?q=ip%3a$1&first=$i1" -s |  grep -Po "(?<=<a href=\").*?(?= h=)" | grep -Po "(?<=://).*?(?=/)" | egrep -v "microsoft"; done >> /tmp/domains.txt
+	nmap -p443 --script ssl-cert $1 | grep -Po "(?<=commonName=).*?(?=/)"
 	sort -u /tmp/domains.txt
  }
 
