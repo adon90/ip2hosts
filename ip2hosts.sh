8fb6a548e47724aa7143bffb0596a78d61542f67
Daniel Martinez Adan <dmartinez@sia.es>
2017-10-26 11:37:08 +0200

    fixed robtex, added 2 sources (threadcrowd and hackertarget

--- ip2hosts.sh
+++ ip2hosts.sh
@@ -4,16 +4,19 @@ if [ "$#" -ne 1 ]; then
 fi
 ip2hosts() { 
 
-	rm /tmp/domains.txt 2>/dev/null
-	curl https://www.robtex.com/ip-lookup/$1 --silent  | grep "xsha" | grep -Po "(?<=dns-lookup/).*?(?=\">)" >> /tmp/domains.txt
+	rm /tmp/domains.txt  2>/dev/null
+	curl https://www.robtex.com/ip-lookup/$1 --silent  | grep "brx" | grep -Po "(?<=dns-lookup/).*?(?=\">)" >> /tmp/domains.txt
 	curl "http://www.virustotal.com/vtapi/v2/ip-address/report?ip=$1&apikey=3c052e9a7339f3a73f00bd67baea747e47f59ee6c1596e59590fd953d00ce519" -s | json_pp | grep -Po "(?<=\"hostname\" : \").*?(?=\",)" >> /tmp/domains.txt
-        dig +short -x $1 >> /tmp/domains.txt
+	dig +short -x $1 >> /tmp/domains.txt
 	shodan host $1 2>/dev/null | grep -Po "(?<=Hostnames:\s).*" | tr -d " " >> /tmp/domains.txt
 	for i in {0..9}; do curl "https://www.bing.com/search?q=ip%3a$1&first=$i1" -s |  grep -Po "(?<=<a href=\").*?(?= h=)" | grep -Po "(?<=://).*?(?=/)" | egrep -v "microsoft|bing|pointdecontact"; done >> /tmp/domains.txt
-	nmap -p443 --script ssl-cert $1 2>/dev/null | grep Subject | grep -Po "(?<=commonName=).*?(?=/)" | tr '[:upper:]' '[:lower:]' >> /tmp/domains.txt
-	curl -X POST -F "remoteAddress=$1"  http://domains.yougetsignal.com/domains.php -s | json_pp | grep -Poz "(?s)\[.*\]" | cat -v | grep -Po "(?<=\").+(?=\")" >> /tmp/domains.txt
+	nmap -p443 --script ssl-cert $1 | grep Subject | grep -Po "(?<=commonName=).*?(?=/)" | tr '[:upper:]' '[:lower:]' >> /tmp/domains.txt
 	sed -i 's/\.$//g' /tmp/domains.txt
+	curl -X POST -F "remoteAddress=$1"  http://domains.yougetsignal.com/domains.php -s | /usr/bin/perl -p | grep -Poz "(?s)\[.*\]" | cat -v | grep -Po "(?<=\").+(?=\")" >> /tmp/domains.txt
+	curl -i -s -k  -X 'POST' -F "theinput=$1" -F "thetest=reverseiplookup" -F "name_of_nonce_field=23gk"    'https://hackertarget.com/reverse-ip-lookup/' | grep -Poz "(?s)(?<=<pre id=\"formResponse\">).*(?=</pre>)" >> /tmp/domains.txt
+	curl -i -s -k  -X 'GET' 'https://www.threatcrowd.org/graphHtml.php?ip=$1' | grep -Po "(?<=source: ').*?(?=')" | egrep -v ^[0-9] | sort -u >> /tmp/domains.txt
 	sort -u /tmp/domains.txt
+
  }
 
 ip2hosts $1
